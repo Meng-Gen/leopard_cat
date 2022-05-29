32533b56ebdd8ecf5dfeec3531719cd466c275ce
Meng-Gen <plover@gmail.com>
2022-05-29 21:43:43 +0800

    Setup packages

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""Set up leopard_cat package."""
+
+from setuptools import setup
+
+
+setup(name='leopard_cat',
+      version='1.0',
+      author='Meng-Gen Tsai',
+      author_email = 'plover@gmail.com',
+      description = 'Leopard Cat Python Package',
+      packages=['leopard_cat'])
