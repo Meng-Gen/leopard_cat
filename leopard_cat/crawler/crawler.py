69e12a2c0b94e8d1fd198b5c5a361609e1771f17
Meng-Gen <plover@gmail.com>
2022-05-30 11:36:44 +0800

    Logging

--- leopard_cat/crawler/crawler.py
+++ leopard_cat/crawler/crawler.py
@@ -1,5 +1,7 @@
 """Module to crawl feed sources."""
 
+import logging
+
 from leopard_cat.crawler.feed_source import FeedSource
 
 
@@ -12,4 +14,4 @@ class Crawler():
     Args:
       feed_source: The feed source to be crawled.
     """
-    print(feed_source)
+    logging.debug('Crawling %r feed source', feed_source)
